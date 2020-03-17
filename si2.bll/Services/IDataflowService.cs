fcf7123a6be3590f9684deeef2c17518b6c2a30a
Sassine Aboujaoude <sassine.jaoude@hotmail.com>
2020-03-17 09:56:59 +0200

    Added Validation to httpPatch for dataflow service - added unit test class for dataflow (unused yet)

--- si2.bll/Services/IDataflowService.cs
+++ si2.bll/Services/IDataflowService.cs
@@ -14,7 +14,8 @@ namespace si2.bll.Services
     {
         Task<DataflowDto> CreateDataflowAsync(CreateDataflowDto createDataflowDto, CancellationToken ct);
         Task<DataflowDto> UpdateDataflowAsync(Guid id, UpdateDataflowDto updateDataflowDto, CancellationToken ct);
-        Task<DataflowDto> PartialUpdateDataflowAsync(Guid id, JsonPatchDocument<UpdateDataflowDto> patchDoc, CancellationToken ct);
+        Task<DataflowDto> PartialUpdateDataflowAsync(Guid id, UpdateDataflowDto patchDoc, CancellationToken ct);
+        Task<UpdateDataflowDto> GetUpdateDataFlowDto(Guid id, CancellationToken ct);
         Task<DataflowDto> GetDataflowByIdAsync(Guid id, CancellationToken ct);
         Task DeleteDataflowByIdAsync(Guid id, CancellationToken ct);
         Task<PagedList<DataflowDto>> GetDataflowsAsync (DataflowResourceParameters pagedResourceParameters, CancellationToken ct);
---
+++ si2.tests/Services/DataFlowTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using si2.bll.Helpers.Credits;
+using si2.dal.Entities;
+using si2.common;
+
+namespace si2.tests.Helpers
+{
+    [TestFixture]
+    class DataFlowTest
+    {
+
+        
+    }
+}
