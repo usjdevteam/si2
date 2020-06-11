b2cdad6407a07808964f91444dccba2d27aac1f3
Tania Estephan <tania.estephan@usj.edu.lb>
2020-06-11 18:05:48 +0300

    Added ParentChild attribute under Institution entity

--- si2.dal/Entities/Institution.cs
+++ si2.dal/Entities/Institution.cs
@@ -25,11 +25,11 @@ namespace si2.dal.Entities
         [Required]
         [ForeignKey("ContactInfo")]
         public Guid ContactInfoId { get; set; }
-        [ForeignKey("Institution")]
-        public Guid ParentInstitutionId { get; set; }
-
         public virtual Address Address { get; set; }
         public virtual ContactInfo ContactInfo { get; set; }
-        //public virtual Institution InstitutionAttr { get; set; }
+        /* [ForeignKey("Institution")]
+         public Guid? ParentInstitutionId { get; set; }*/
+        public virtual Institution Parent { get; set; }
+        public virtual ICollection<Institution> Children { get; set; }
     }
 }
---
+++ si2.dal/Migrations/20200611140614_Added_ParentChildren.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace si2.dal.Migrations
+{
+    public partial class Added_ParentChildren : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentInstitutionId",
+                table: "Institution");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentId",
+                table: "Institution",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Institution_ParentId",
+                table: "Institution",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Institution_Institution_ParentId",
+                table: "Institution",
+                column: "ParentId",
+                principalTable: "Institution",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Institution_Institution_ParentId",
+                table: "Institution");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Institution_ParentId",
+                table: "Institution");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Institution");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentInstitutionId",
+                table: "Institution",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
