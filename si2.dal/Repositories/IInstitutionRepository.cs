d5de3d9376859cb692e83c9b9a9e250f22e840ce
Marie Kassis <marie.kassis@usj.edu.lb>
2020-06-16 13:58:35 +0300

    Added the institution repository

---
+++ si2.dal/Repositories/IInstitutionRepository.cs
@@ -0,0 +1,11 @@
+﻿using si2.dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace si2.dal.Repositories
+{
+    public interface IInstitutionRepository : IRepository<Institution>
+    {
+    }
+}
