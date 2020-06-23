648c87d3b0a5457904d34de3847f989526de1d4c
Tania Estephan <tania.estephan@usj.edu.lb>
2020-06-23 08:44:16 +0300

    Removed the overridden Equal function from Dtos

--- si2.bll/Dtos/Results/ContactInfo/ContactInfoDto.cs
+++ si2.bll/Dtos/Results/ContactInfo/ContactInfoDto.cs
@@ -10,14 +10,14 @@ namespace si2.bll.Dtos.Results.ContactInfo
         public string Phone { get; set; }
         public string Fax { get; set; }
         public byte[] RowVersion { get; set; }
-        public override bool Equals(Object obj) => Equals(obj as ContactInfoDto);
+        /*public override bool Equals(Object obj) => Equals(obj as ContactInfoDto);
 
         public bool Equals(ContactInfoDto obj)
         {
             return (string.Equals(this.Email, obj.Email, StringComparison.OrdinalIgnoreCase)
                 && string.Equals(this.Phone, obj.Phone, StringComparison.OrdinalIgnoreCase)
                 && string.Equals(this.Fax, obj.Fax, StringComparison.OrdinalIgnoreCase));
-        }
+        }*/
 
         public override int GetHashCode()
         {
